43b5c72a4148bb544af23b3f5d5fad99e639687a
SoyeonL <sylee932003@gmail.com>
2023-03-01 10:25:01 -0800

    Server Login Route

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+
+app.post("/login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "b3cf4a0126484d49ae681c685ce8be46",
+    clientSecret: "39ded147ebe64ffdbc06d22e8b5989f6",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
